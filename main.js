08379a2bffa35837a964f85b21f1fc677ba3d64b
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-05-06 11:24:18 -0700

    Fixes alignment and Tweet gubtton logic

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 console.log("JS file is working!");
 
+var currentQuote = $('#quote').text();
+
+function generateQuote(){
+  $.get("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(data, status){
+      currentQuote = data.message;
+      $("#quote").html("\"" + currentQuote + "\"");
+      tweetQuote();
+  });
+}
+
+function tweetQuote (){
+  $("#tweetThis").on("click", function(event){
+    var url = 'https://twitter.com/share?text=' +
+      encodeURIComponent(currentQuote);
+    console.log(url);
+    $(this).attr({
+      "href": url,
+      "text": currentQuote
+    });
+  });
+}
 
 $(document).ready(function(){
-  var currentQuote = "";
+  generateQuote();
   $("#getQuote").on("click", function(){
-    $.get("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(data, status){
-        currentQuote = data.message;
-        $("#quote").html("\"" + currentQuote + "\"");
-    });
-    $("#tweetThis").removeClass("hidden");
-    $("#tweetThis").on("click", function(){
-      var url = "https://twitter.com/intent/tweet?text=" + currentQuote + "";
-      console.log(url);
-      $(this).attr("href", url);
-    });
+    generateQuote();
   });
 });
